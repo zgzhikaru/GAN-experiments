27a722cabca1b07a776308d63e8667dfa77cc7ec
Guangzheng Zhang <zgzhikaru97@gmail.com>
2025-08-04 11:21:15 +0800

    Organize config files

--- script.sh
+++ script.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-#python3 src/main.py -t -metrics fid is -cfg src/configs/LSUN_bedroom/BigGAN-64Res.yaml -data data/lsun_bedroom/ -save results/ -DDP -sync_bn
-python3 src/main.py -t -metrics fid is -cfg src/configs/Flowers102/BigGAN-Res256-Uncond.yaml -data data/Flowers102/ -save results/ -DDP -sync_bn
+python3 src/main.py -t -metrics fid is --pre_resizer wo_resize -cfg src/configs/Tiny_ImageNet/StyleGAN2-SPD-64.yaml -data data/tiny-imagenet-200 -save results/ -DDP
+python3 src/main.py -t -metrics fid is --pre_resizer wo_resize -cfg src/configs/Tiny_ImageNet/StyleGAN2-SPD-64-Uncond.yaml -data data/tiny-imagenet-200 -save results/ -DDP
